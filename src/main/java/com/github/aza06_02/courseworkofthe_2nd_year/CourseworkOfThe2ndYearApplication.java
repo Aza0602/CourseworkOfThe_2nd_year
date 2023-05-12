cef8b27c8d21ce6a05e8e06e885b182f271ce654
Azat <aelchikushuulu@gmail.com>
2023-05-12 21:57:05 +0600

    Initial commit

---
+++ src/main/java/com/github/aza06_02/courseworkofthe_2nd_year/CourseworkOfThe2ndYearApplication.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.courseworkofthe_2nd_year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkOfThe2ndYearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkOfThe2ndYearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/aza06_02/courseworkofthe_2nd_year/CourseworkOfThe2ndYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.courseworkofthe_2nd_year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkOfThe2ndYearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
